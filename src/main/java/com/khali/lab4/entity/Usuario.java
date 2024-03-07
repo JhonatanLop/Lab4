3b6590776d1c6b411de759fa2e95328a8bc24a10
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-03-06 21:46:33 -0300

    Add Anotacao and Autorizacao entities and their repositories

--- src/main/java/com/khali/lab4/entity/Usuario.java
+++ src/main/java/com/khali/lab4/entity/Usuario.java
@@ -1,17 +1,26 @@
 package com.khali.lab4.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
@@ -29,5 +38,35 @@ public class Usuario {
 
     @Column(name = "usr_password")
     private String senha;
-    
+
+    @OneToMany(mappedBy = "usuario")
+    @JsonIgnore
+    private Set<Anotacao> anotacoes;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = { @JoinColumn(name = "usr_id")},
+        inverseJoinColumns = { @JoinColumn(name = "aut_id") }
+        )
+    private Set<Autorizacao> autorizacoes;
+
+    public Usuario(String nome, String senha){
+        this();
+        setNome(nome);
+        setSenha(senha);
+    }
+
+    public Usuario(){
+        setAnotacoes(new HashSet<Anotacao>());
+        setAutorizacoes(new HashSet<Autorizacao>());
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public Set<Anotacao> getAnotacoes() { return anotacoes; }
+    public void setAnotacoes(Set<Anotacao> anotacoes) { this.anotacoes = anotacoes; }
 }
---
+++ src/main/java/com/khali/lab4/repository/Autorizacao.java
@@ -0,0 +1,7 @@
+package com.khali.lab4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Autorizacao extends JpaRepository<Autorizacao, Long>{
+    
+}
